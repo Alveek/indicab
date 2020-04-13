bb5e52dbf5582da33d0e2536bd6abc01404f0a8d
Alveek <alveek@gmail.com>
2020-04-13 06:08:22 +0300

    cancel project massage

--- App.svelte
+++ App.svelte
@@ -1,3 +1,4 @@
+<!--
 <script>
 	import { auth } from "./firebase.js";
 	import SupplyList from "./components/SupplyList.svelte";
@@ -89,3 +90,6 @@
 				  } */
 		}
 </style>
+-->
+
+<h2>Проект удален.</h2>
