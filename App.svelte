ff079f318132ca2d593bdbfd644f475a34433796
Alveek <alveek@gmail.com>
2020-04-16 11:29:37 +0300

    Open again

--- App.svelte
+++ App.svelte
@@ -1,4 +1,4 @@
-<!--
+
 <script>
 	import { auth } from "./firebase.js";
 	import SupplyList from "./components/SupplyList.svelte";
@@ -73,6 +73,4 @@
 		
 		}
 </style>
--->
 
-<h2>Программа закрыта</h2>
